03e5899cf04e99a87b1585796c5945d8cddf499d
Esso <marc.esso01@gmail.com>
2017-09-01 13:19:49 +0300

    Editing the About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,17 @@ var React = require('react');
 var About = (props) => {
     return(
             <div>
-            <h3>About</h3>
-            <p>This is the About paragraph</p>
+            <h1 className="text-center">About</h1>
+            <p>This is the About paragraph, we are still developing our application, if you want to contact us, do not hesitate to send an email to: marc.esso01@gmail.com</p>
+            <p>Here are some of the tools i used:</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - This was the Javascript Framework used.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map </a> - I used openWeatherMap API to ge the data.
+                </li>
+            </ul>
             </div>
         );
 };
