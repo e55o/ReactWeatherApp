78a1f6dcdb5e7f6289dc4551450b0457775a05a9
Esso <marc.esso01@gmail.com>
2017-09-01 15:11:52 +0300

    Switch to SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
             WeatherForm: 'app/components/WeatherForm.jsx',
             WeatherMessage: 'app/components/WeatherMessage',
             openWeatherMap:'app/api/openWeatherMap.jsx',
-            ErrorModal:'app/components/ErrorModal.jsx'
+            ErrorModal:'app/components/ErrorModal.jsx',
+            applicationStyle: 'app/styles/app.scss'
         },
         extensions: ['', '.js', '.jsx']
     },
